9998cd720775c4ff825d9ced5b6d8fe09505a835
nexxxter <ilya_gubin98@mail.ru>
2022-09-02 15:49:10 +0300

    Initial Commit

---
+++ app.py
@@ -0,0 +1,37 @@
+import telebot
+from config import keys, TOKEN
+from extensions import APIException, CryptoConverter
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message: telebot.types.Message):
+    text = 'Вас приветствует xBot, обладающий следующим функционалом:  \n- Показать список доступных для конвертации валют через команду \n /values \
+    \n- Вывести конвертацию валюты через команду <имя валюты> <имя валюты, в которую хотите перевести> <количество переводимой валюты>\n \
+- Помощь, через команду /help'
+    bot.reply_to(message, text)
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for key in keys.keys():
+        text = '\n'.join((text, key, ))
+    bot.reply_to(message, text)
+
+@bot.message_handler(content_types=['text',])
+def get_price(message: telebot.types.Message):
+    try:
+        values = message.text.split(' ')
+        if len(values) != 3:
+            raise APIException('Введите 3 паараметра или команду')
+        quote, base, amount = values
+        total_base = CryptoConverter.get_price(quote, base, amount)
+    except APIException as e:
+        bot.reply_to(message, f'Ошибка пользователя.\n{e}')
+    except Exception as e:
+        bot.reply_to(message, f'Не удалось обработать команду\n{e}')
+    else:
+        text = f'Цена {amount} {quote} в {base} = {total_base}'
+        bot.send_message(message.chat.id, text)
+
+bot.polling()
---
+++ config.py
@@ -0,0 +1,6 @@
+TOKEN = '5781518251:AAFDYfVzLgW01oz5vVYrKkhy8Ctt_6vXar8'
+keys = {
+    'Евро' : 'EUR',
+    'Доллар' : 'USD',
+    'Рубль' : 'RUB'
+}
